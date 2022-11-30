8e7393738339308f9a8cf835719cff30e803e4db
patic457 <112005137+patic457@users.noreply.github.com>
2022-12-01 00:17:49 +0700

    firs

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,5 @@
 import 'package:soso_mobile_cart/cart/presentation/pages/cart_page.dart';
+import 'package:soso_mobile_cart/cart/presentation/pages/checkout_page.dart';
 
 class Config {
   static String sosoRoute = '/cart';
